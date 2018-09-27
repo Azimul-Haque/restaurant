fdab2c626a3c7344ae76cdae6cb2a7e749801419
Orbachin Ujbuk <orbachinujubk@gmail.com>
2018-09-27 06:10:41 +0600

    Commodity Error Fixed!

--- app/Http/Controllers/CommodityController.php
+++ app/Http/Controllers/CommodityController.php
@@ -26,16 +26,9 @@ class CommodityController extends Controller
      */
     public function index()
     {
-        try {
-          $commodities = Commodity::orderBy('created_at', 'desc')->get();
-          $categories = Category::all();
-          $sources = Source::all();
-        }
-        catch (\Exception $e) {
-          $commodities = null;
-          $categories = null;
-          $sources = null;
-        }
+        $commodities = Commodity::orderBy('created_at', 'desc')->get();
+        $categories = Category::all();
+        $sources = Source::all();
 
         return view('commodities.index')
                     ->withCommodities($commodities)
