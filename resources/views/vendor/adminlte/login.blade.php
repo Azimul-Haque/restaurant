264d64acd4a42241d6d8e8e71b64c93e9166b19f
Orbachin Ujbuk <orbachinujubk@gmail.com>
2018-10-08 04:52:25 +0600

    Version 1.2.1 Release!

--- resources/views/vendor/adminlte/login.blade.php
+++ resources/views/vendor/adminlte/login.blade.php
@@ -11,6 +11,7 @@
 @section('body')
     <div class="login-box">
         <div class="login-logo">
+            <img src="{{ asset('images/icon.png') }}" style="max-width: 70px; height: auto;">
             <a href="{{ url(config('adminlte.dashboard_url', 'dashboard')) }}">{!! config('adminlte.logo', '<b>QueenIsland</b>Kitchen') !!}</a>
         </div>
         <!-- /.login-logo -->
